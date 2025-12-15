674be7de16487cea0701b101995a3902cfc4d400
Raika-de-Paula <depaularaika@gmail.com>
2025-12-15 10:17:10 -0300

    pc3

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,3 +1,4 @@
+//middleware/authMiddleware
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Carrega as variáveis de ambiente do .env
+//server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
